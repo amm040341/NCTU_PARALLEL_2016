33d83db99e3d2625b58f7401a634ee40994b5591
jubeatWwW <l6104400@gmail.com>
2016-10-21 20:58:42 +0800

    separate main program and test program, add test script to makefile

--- hw1/piTest.cpp
+++ hw1/piTest.cpp
@@ -8,9 +8,12 @@ using namespace std;
 #define MAX numeric_limits<uint64_t>::max() 
 #define MIN numeric_limits<uint64_t>::min() 
 #define SQUARE_DIS(x,y) x*x+y*y
+//#define CORE 8
 
-void *MC(void* n){
+void *msg(void* n){
     long times = (long)n;
+    //printf("times: %ld\n", times);
+    //printf("pid: %d\n", (int)pthread_self());
     mt19937 generator(time(0)+(int)pthread_self());
     uniform_int_distribution<uint64_t> distribution(MIN, MAX);
     long dart_cnt = 0;
@@ -25,7 +28,10 @@ void *MC(void* n){
 }
 
 int main(int argc, char** argv){
-    int CORE = sysconf(_SC_NPROCESSORS_ONLN);
+    //int CORE = sysconf(_SC_NPROCESSORS_ONLN);
+    int CORE = atoi(argv[2]);
+    printf("core: %d\n", CORE);
+    cout << "cnt:" << argv[1] << endl;
     long cnt = atoi(argv[1]);
     pthread_t threads[CORE];   
     for(int i=0; i<CORE; i++){
@@ -33,18 +39,20 @@ int main(int argc, char** argv){
         if(i==CORE-1){
             times += cnt%CORE;
         }
-        pthread_create(&threads[i], NULL, MC, (void*)times);
+        pthread_create(&threads[i], NULL, msg, (void*)times);
     }
     long long int sum = 0;
     for(int i=0; i<CORE; i++){
         void *tmp;
         pthread_join(threads[i], &tmp);
         long a = (long)tmp;
+        printf("hit time: %d\n", a);
         sum+=a;
     }
 
+    printf("sum: %ld\n", sum);
     double estimate_pi = 4*sum/(double)cnt;
-    printf("%.8lf\n", estimate_pi);
+    printf("pi: %.8lf\n", estimate_pi);
 }
 
 
