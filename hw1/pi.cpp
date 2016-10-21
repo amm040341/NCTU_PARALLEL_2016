9448b91e41ed5748c1aae82c7e7bb21179557950
jubeatWwW <l6104400@gmail.com>
2016-10-21 18:06:30 +0800

    able to run but have some strange bug

--- hw1/pi.cpp
+++ hw1/pi.cpp
@@ -5,29 +5,52 @@
 #include <unistd.h>
 #include <random>
 using namespace std;
-
-double Rand(const uint64_t & min, const uint64_t & max) {
-    static mt19937 generator;
-    uniform_int_distribution<uint64_t> distribution(min,max);
-    return (double)distribution(generator)/((double)max/2) - 1;
-}
+#define MAX numeric_limits<uint64_t>::max() 
+#define MIN numeric_limits<uint64_t>::min() 
+#define SQUARE_DIS(x,y) x*x+y*y
+#define CORE 1
 
 void *msg(void* n){
-    int times = *((int*)n);
-    while(1)
-        printf("%.20lf\n", Rand(0, numeric_limits<uint64_t>::max()));
+    long times = (long)n;
+    printf("times: %ld\n", times);
+    static mt19937 generator;
+    uniform_int_distribution<uint64_t> distribution(MIN, MAX);
+    long dart_cnt = 0;
+    for(int i=0; i< times; i++){
+        long double x = (long double)distribution(generator)/((long double)MAX/2) - 1;
+        long double y = (long double)distribution(generator)/((long double)MAX/2) - 1;
+        if(SQUARE_DIS(x, y) <= 1){
+            dart_cnt++;
+        }
+    }
+    return (void *)dart_cnt;
 }
 
 int main(int argc, char** argv){
-    int id[4] = {1, 2, 3, 4};
+    int core =sysconf(_SC_NPROCESSORS_ONLN); 
+    printf("core: %d\n", core);
     cout << "cnt:" << argv[1] << endl;
-    pthread_t threads[4];   
-    for(int i=0; i<4; i++){
-        pthread_create(&threads[i], NULL, msg, (void*)(id+i));
+    long cnt = atoi(argv[1]);
+    pthread_t threads[CORE];   
+    for(int i=0; i<CORE; i++){
+        long times = cnt/CORE;
+        if(i==CORE-1){
+            times += cnt%CORE;
+        }
+        pthread_create(&threads[i], NULL, msg, (void*)times);
     }
-    for(int i=0; i<4; i++){
-        pthread_join(threads[i], NULL);
+    long long int sum = 0;
+    for(int i=0; i<CORE; i++){
+        void *tmp;
+        pthread_join(threads[i], &tmp);
+        long a = (long)tmp;
+        printf("hit time: %d\n", a);
+        sum+=a;
     }
+
+    printf("sum: %ld\n", sum);
+    double estimate_pi = 4*(double)sum/(double)cnt;
+    printf("pi: %.8lf\n", estimate_pi);
 }
 
 
