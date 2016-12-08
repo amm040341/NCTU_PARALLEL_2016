cbc0631f4b29d21c3328a657972f91f4b1461709
JubeatWwW <l6104400@gmail.com>
2016-12-09 01:10:59 +0800

    simple

---
+++ project4/wave.cu
@@ -0,0 +1,163 @@
+/**********************************************************************
+ * DESCRIPTION:
+ *   Serial Concurrent Wave Equation - C Version
+ *   This program implements the concurrent wave equation
+ *********************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+#define MAXPOINTS 1000000
+#define MAXSTEPS 1000000
+#define MINPOINTS 20
+#define PI 3.14159265
+
+void check_param(void);
+void init_line(void);
+void update (void);
+void printfinal (void);
+
+int nsteps,                 	/* number of time steps */
+    tpoints, 	     		/* total points along string */
+    rcode;                  	/* generic return code */
+float  values[MAXPOINTS+2], 	/* values at time t */
+       oldval[MAXPOINTS+2], 	/* values at time (t-dt) */
+       newval[MAXPOINTS+2]; 	/* values at time (t+dt) */
+
+/**********************************************************************
+ *	Checks input values from parameters
+ *********************************************************************/
+void check_param(void)
+{
+   char tchar[20];
+
+   /* check number of points, number of iterations */
+   while ((tpoints < MINPOINTS) || (tpoints > MAXPOINTS)) {
+      printf("Enter number of points along vibrating string [%d-%d]: "
+           ,MINPOINTS, MAXPOINTS);
+      scanf("%s", tchar);
+      tpoints = atoi(tchar);
+      if ((tpoints < MINPOINTS) || (tpoints > MAXPOINTS))
+         printf("Invalid. Please enter value between %d and %d\n", 
+                 MINPOINTS, MAXPOINTS);
+   }
+   while ((nsteps < 1) || (nsteps > MAXSTEPS)) {
+      printf("Enter number of time steps [1-%d]: ", MAXSTEPS);
+      scanf("%s", tchar);
+      nsteps = atoi(tchar);
+      if ((nsteps < 1) || (nsteps > MAXSTEPS))
+         printf("Invalid. Please enter value between 1 and %d\n", MAXSTEPS);
+   }
+
+   printf("Using points = %d, steps = %d\n", tpoints, nsteps);
+
+}
+
+/**********************************************************************
+ *     Initialize points on line
+ *********************************************************************/
+void init_line(void)
+{
+   int i, j;
+   float x, fac, k, tmp;
+
+   /* Calculate initial values based on sine curve */
+   fac = 2.0 * PI;
+   k = 0.0; 
+   tmp = tpoints - 1;
+   for (j = 1; j <= tpoints; j++) {
+      x = k/tmp;
+      values[j] = sin (fac * x);
+      k = k + 1.0;
+   } 
+
+   /* Initialize old values array */
+   for (i = 1; i <= tpoints; i++) 
+      oldval[i] = values[i];
+}
+
+/**********************************************************************
+ *      Calculate new values using wave equation
+ *********************************************************************/
+__global__ void do_math(float* Cvalues, float* Coldval, float* Cnewval, int nstep)
+{
+    int idx =blockIdx.x;
+    float dtime, c, dx, tau, sqtau;
+
+    dtime = 0.3;
+    c = 1.0;
+    dx = 1.0;
+    tau = (c * dtime / dx);
+    sqtau = tau * tau;
+    int i;
+    for(i = 1; i<=nstep; i++){
+        Cnewval[idx] = (2.0 * Cvalues[idx]) - Coldval[idx] + (sqtau *  (-2.0)*Cvalues[idx]);
+        Coldval[idx] = Cvalues[idx];
+        Cvalues[idx] = Cnewval[idx];
+    }
+}
+
+/**********************************************************************
+ *     Update all values along line a specified number of times
+ *********************************************************************/
+void update()
+{
+    int i;
+
+    /* Update values for each time step */
+    float *Cnewval, *Cvalues, *Coldval;
+    int data_size = (MAXPOINTS+2)*sizeof(float);
+    cudaMalloc((void**)&Cnewval, data_size);
+    cudaMalloc((void**)&Cvalues, data_size);
+    cudaMalloc((void**)&Coldval, data_size);
+    
+
+    cudaMemcpy( Cvalues, values, data_size, cudaMemcpyHostToDevice);
+    cudaMemcpy( Coldval, oldval, data_size, cudaMemcpyHostToDevice);
+    do_math<<<tpoints, 1>>>(Cvalues, Coldval, Cnewval, nsteps);
+
+    //cudaMemcpy( oldval, Cvalues, data_size, cudaMemcpyDeviceToHost );
+    cudaMemcpy( values, Cnewval, data_size, cudaMemcpyDeviceToHost );
+        
+        
+    values[0] = 0.0;
+    values[tpoints] = 0.0;
+    
+    cudaFree(Cnewval);
+    cudaFree(Coldval);
+    cudaFree(Cvalues);
+}
+
+/**********************************************************************
+ *     Print final results
+ *********************************************************************/
+void printfinal()
+{
+   int i;
+
+   for (i = 1; i <= tpoints; i++) {
+      printf("%6.4f ", values[i]);
+      if (i%10 == 0)
+         printf("\n");
+   }
+}
+
+/**********************************************************************
+ *	Main program
+ *********************************************************************/
+int main(int argc, char *argv[])
+{
+    sscanf(argv[1],"%d",&tpoints);
+	sscanf(argv[2],"%d",&nsteps);
+	check_param();
+	printf("Initializing points on the line...\n");
+	init_line();
+	printf("Updating all points for all time steps...\n");
+	update();
+	printf("Printing final results...\n");
+	printfinal();
+	printf("\nDone.\n\n");
+	
+	return 0;
+}
